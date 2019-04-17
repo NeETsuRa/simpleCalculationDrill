f22874245f173292dd4beff4e5122c641287fc7b
Nejc <dev.neetsu@gmail.com>
2019-04-18 00:31:45 +0200

    Add drill class

--- app/src/main/java/ravnjak/nejc/deljenje.java
+++ app/src/main/java/ravnjak/nejc/deljenje.java
@@ -13,7 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
+// drill chalange
 public class deljenje extends Activity {
 	int napake=0;
 	int pravilni=0;
@@ -22,20 +22,23 @@ public class deljenje extends Activity {
 	int c=0;
 	
 	@Override
-	    public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set the view
         setContentView(R.layout.program);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Start the drill
         naloga();
     }
+
 	public void preveri (View v) {
+		//check the final result
 		TextView nal = (TextView)findViewById(R.id.hint);
         nal.setTextColor(Color.WHITE);
         nal.setTextSize(1);
         
 		EditText resitev=(EditText)findViewById(R.id.rezultat);
-		
-			res=Integer.parseInt(resitev.getText().toString());
+		res=Integer.parseInt(resitev.getText().toString());
 		
 		if(res==c)
 			pravilni++;
@@ -45,48 +48,41 @@ public class deljenje extends Activity {
 		if(st_nalog==10){
 			konec();
 			return;
-					}
+		}
 		
 		naloga();
 	}
 	
 	public void konec(){
+		// present the finall result
 		setContentView(R.layout.statistika);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		TextView sta = (TextView)findViewById(R.id.statistika);
         sta.setTextColor(Color.BLACK);
         sta.setText("Pravilni: "+String.valueOf(pravilni)+" \n "+"Napaèni: " + String.valueOf(napake));
-        
-		
 	}
 	
 	public void nazaj(View v){
+		// go back
 		startActivity(new Intent("ravnjak.nejc.naslovnica"));
 		finish();
 	}
-	
-	
-	
-	
-	
+
 	public void naloga(){
-		
+		// the main function for the drill calculation
 		final Random naklju = new Random();
         int a=naklju.nextInt(100);
         int b=naklju.nextInt(100);
         EditText resitev=(EditText)findViewById(R.id.rezultat);
         resitev.setText("0");
-        
-        	
-        
+
         TextView nal = (TextView)findViewById(R.id.racun);
         nal.setTextColor(Color.BLACK);
       
         	c=a*b;
         	nal.setText(String.valueOf(c)+":"+String.valueOf(b)+"=");
         	c=a;
-        	
-        	
+
         Button gumb = (Button) findViewById(R.id.button1);
         gumb.setOnLongClickListener(new View.OnLongClickListener() {
 			
@@ -98,7 +94,5 @@ public class deljenje extends Activity {
 				return false;
 			}
 		});
-        	
 	}
-
 }
