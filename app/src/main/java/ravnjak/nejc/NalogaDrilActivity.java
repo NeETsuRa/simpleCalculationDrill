514f3ef3eb28ba63fbe3b625c1c1795f236c3364
Nejc <dev.neetsu@gmail.com>
2019-04-17 21:13:05 +0200

    Comment the initial site of the application

--- app/src/main/java/ravnjak/nejc/NalogaDrilActivity.java
+++ app/src/main/java/ravnjak/nejc/NalogaDrilActivity.java
@@ -10,33 +10,31 @@ public class NalogaDrilActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set the intro banner
         setContentView(R.layout.main);
+        // define a thread so all the precesses can load
         Thread nit = new Thread(){
         	@Override
 			public void run(){
         		try{
         			sleep(3000);
+        			//After 3s load the main site
         			startActivity(new Intent("ravnjak.nejc.naslovnica"));
         		} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}finally{
 					finish();
-        			
         		}
         	}
         	
         };
-        
+
+        // run the thread
         nit.start();
     }
 
 	@Override
 	protected void onDestroy() {
-		// TODO Auto-generated method stub
 		super.onDestroy();
 	}
-    
-    
-
 }
